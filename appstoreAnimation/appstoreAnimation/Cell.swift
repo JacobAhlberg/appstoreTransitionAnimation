d4e17c3e6e2e72f24dec8392eb2bc07daee7dac2
Jacob Ahlberg <jacahlberg@hotmail.com>
2018-11-10 14:28:24 +0100

    First commit

---
+++ appstoreAnimation/appstoreAnimation/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  appstoreAnimation
+//
+//  Created by Jacob Ahlberg on 2018-11-10.
+//  Copyright © 2018 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+}
